3f9371ccb022547cefc9c12a46eaabe0e214fd6a
jazzpi <jasper@mezzo.de>
2015-07-27 19:58:34 +0200

    Problem 5

---
+++ problem-5/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# No programming required - just take all the primes in 1-20 and multiply their
+# highest powers that is less than 20 together
+#
+# Yes I know that sounds shitty but I don't care
+print(2 ** 4 * 3 ** 2 * 5 * 7 * 11 * 13 * 17 * 19)
